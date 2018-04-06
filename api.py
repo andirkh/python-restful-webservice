93c890aee908d259ca236520f9092cd2277d722c
Andi <andi@Niltavas-Mac-mini.local>
2018-04-06 14:50:21 +0700

    get one user

--- api.py
+++ api.py
@@ -53,9 +53,20 @@ def get_all_users():
 	# output
 	return jsonify({ 'users' : output})
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-	return ''
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+	# process :
+	user = User.query.filter_by(public_id=public_id).first()
+
+	if not user:
+		return jsonify({'message': 'No user found!'})
+
+	user_data = {}
+	user_data['public_id'] = user.public_id
+	user_data['name'] = user.name
+	user_data['password'] = user.password
+	user_data['admin'] = user.admin
+	return jsonify({'user': user_data})
 
 @app.route('/user', methods=['POST'])
 def create_user():
